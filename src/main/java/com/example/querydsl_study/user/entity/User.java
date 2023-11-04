a42747ef33b67a14e07384bb2b141d201de1f110
Min <als904204@naver.com>
2023-11-04 20:10:16 +0900

    User to Order 연관관계

--- src/main/java/com/example/querydsl_study/user/entity/User.java
+++ src/main/java/com/example/querydsl_study/user/entity/User.java
@@ -1,10 +1,15 @@
 package com.example.querydsl_study.user.entity;
 
+import com.example.querydsl_study.order.entity.Order;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -26,6 +31,9 @@ public class User {
 
     @Column(unique = true)
     private String email;
+
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
+    private List<Order> orderList = new ArrayList<>();
 
 
     @Builder
